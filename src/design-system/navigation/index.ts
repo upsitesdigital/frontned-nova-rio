0303752b80bfd106e033b987c7a39676c50f69f3
Barba2k2 <lorenzo.dz@hotmail.com>
2026-02-28 19:22:46 -0300

    feat: add user menu navigation components

--- src/design-system/navigation/index.ts
+++ src/design-system/navigation/index.ts
@@ -3,3 +3,5 @@ export { DsSidebarItem, type DsSidebarItemProps } from "./ds-sidebar-item";
 export { DsTopbar, type DsTopbarProps } from "./ds-topbar";
 export { DsNavLink, type DsNavLinkProps } from "./ds-nav-link";
 export { DsLogo, type DsLogoProps } from "./ds-logo";
+export { DsUserMenu, type DsUserMenuProps } from "./ds-user-menu";
+export { DsUserMenuItem, type DsUserMenuItemProps } from "./ds-user-menu-item";
