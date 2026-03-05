9301e25881e6a972392859758c13923da71254ae
Barba2k2 <lorenzo.dz@hotmail.com>
2026-03-05 10:56:26 -0300

    feat: add client login endpoint to auth API

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -11,8 +11,29 @@ interface ClientRegisterResponse {
   message: string;
 }
 
+interface ClientLoginRequest {
+  email: string;
+  password: string;
+}
+
+interface TokenPair {
+  accessToken: string;
+  refreshToken: string;
+}
+
 async function registerClient(data: ClientRegisterRequest): Promise<ClientRegisterResponse> {
   return httpPost<ClientRegisterResponse>("/auth/client/register", data);
 }
 
-export { registerClient, type ClientRegisterRequest, type ClientRegisterResponse };
+async function loginClient(data: ClientLoginRequest): Promise<TokenPair> {
+  return httpPost<TokenPair>("/auth/client/login", data);
+}
+
+export {
+  registerClient,
+  loginClient,
+  type ClientRegisterRequest,
+  type ClientRegisterResponse,
+  type ClientLoginRequest,
+  type TokenPair,
+};
