37ca849b76d4a0b7347439c2bf7d19460d5577b1
Barba2k2 <lorenzo.dz@hotmail.com>
2026-03-11 20:31:00 -0300

    refactor: remove mock data layer and config

--- src/api/scheduling-data.ts
+++ src/api/scheduling-data.ts
@@ -1,20 +1,12 @@
-import { appConfig } from "@/config/app";
-import { fetchMockCoverageByCep, fetchMockTimeSlots } from "@/mocks/scheduling-mock";
 import type { CoverageResult, TimeSlot } from "@/types/scheduling";
 
 import { fetchCoverageByCep, fetchTimeSlots } from "./scheduling-api";
 
 async function getTimeSlots(date: string): Promise<TimeSlot[]> {
-  if (appConfig.useMockData) {
-    return fetchMockTimeSlots();
-  }
   return fetchTimeSlots(date);
 }
 
 async function getCoverageByCep(cep: string): Promise<CoverageResult> {
-  if (appConfig.useMockData) {
-    return fetchMockCoverageByCep(cep);
-  }
   return fetchCoverageByCep(cep);
 }
 
