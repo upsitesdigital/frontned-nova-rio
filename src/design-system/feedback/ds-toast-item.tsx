75d403ff0a0e827e0d1687279c9b4c3faf64c30f
Barba2k2 <lorenzo.dz@hotmail.com>
2026-03-12 12:51:46 -0300

    refactor: split ds-toast into ds-toast-item and ds-toast-container

--- src/design-system/feedback/ds-toast-item.tsx
+++ src/design-system/feedback/ds-toast-item.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useRef } from "react";
 
-import { cn } from "@/lib/utils";
 import { useToastStore, type Toast } from "@/stores/toast-store";
 import { DsAlert } from "./ds-alert";
 
@@ -36,18 +35,4 @@ function DsToastItem({ toast }: { toast: Toast }) {
   );
 }
 
-function DsToastContainer() {
-  const toasts = useToastStore((s) => s.toasts);
-
-  if (toasts.length === 0) return null;
-
-  return (
-    <div className={cn("fixed right-6 top-6 z-50 flex w-96 flex-col gap-3")}>
-      {toasts.map((toast) => (
-        <DsToastItem key={toast.id} toast={toast} />
-      ))}
-    </div>
-  );
-}
-
-export { DsToastContainer, DsToastItem };
+export { DsToastItem };
