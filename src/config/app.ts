c0529d2e1a99468bc96b1505a602a98aefeec379
Pedrinho Hilario <flygames1214@gmail.com>
2026-03-16 15:45:02 -0300

    fix: apiBaseUrl

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,7 @@
 const appConfig = {
-  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8080",
+  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://207.244.229.247:4000",
   authCookieName: "nova-rio-auth",
   agendaPageSize: 6,
 } as const;
-
-//mudar url após ajustes
 
 export { appConfig };
