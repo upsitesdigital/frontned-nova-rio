f970cd543b136b152ab4f7ad8997d6df0d0a9923
Barba2k2 <lorenzo.dz@hotmail.com>
2026-04-09 16:22:28 -0300

    fix(config): correct api port to 4000

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,5 @@
 const appConfig = {
-  // apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8080",
+  // apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:4000",
   apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://207.244.229.247:4000",
   authCookieName: "nova-rio-auth",
   agendaPageSize: 6,
